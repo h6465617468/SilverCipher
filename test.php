58464acdc52707a86d10d89d4c154eb50f564b8f
XPROCION <51422382+XPROCION@users.noreply.github.com>
2023-04-12 12:59:35 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,23 +1,23 @@
 <?php
-require_once "AntaresCrypt.php";
+require_once "HiddenTunnel.php";
 $plain_text="Hello World Test 123456789ABC";
 $key="Hello World123456789";
 echo "Plain Text:<br>".$plain_text."<br><br>";
 echo "Example Encrypted Text(1)<br>";
-echo $encrypted_text=AntaresCrypt::Encrypt($plain_text,$key);
+echo $encrypted_text=HiddenTunnel::Encrypt($plain_text,$key);
 echo "<br><br>";
 echo "Example Encrypted Text(2)<br>";
-echo $encrypted_text=AntaresCrypt::Encrypt($plain_text,$key);
+echo $encrypted_text=HiddenTunnel::Encrypt($plain_text,$key);
 echo "<br><br>";
 echo "Example Encrypted Text(3)<br>";
-echo $encrypted_text=AntaresCrypt::Encrypt($plain_text,$key);
+echo $encrypted_text=HiddenTunnel::Encrypt($plain_text,$key);
 echo "<br><br>";
 echo "Example Encrypted Text(4)<br>";
-echo $encrypted_text=AntaresCrypt::Encrypt($plain_text,$key);
+echo $encrypted_text=HiddenTunnel::Encrypt($plain_text,$key);
 echo "<br><br>";
 echo "Example Encrypted Text(5)<br>";
-echo $encrypted_text=AntaresCrypt::Encrypt($plain_text,$key);
+echo $encrypted_text=HiddenTunnel::Encrypt($plain_text,$key);
 echo "<br><br>";
 echo "Decrypted Text:<br>";
-echo $decrypted_text=AntaresCrypt::Decrypt($encrypted_text,$key);
+echo $decrypted_text=HiddenTunnel::Decrypt($encrypted_text,$key);
 ?>
