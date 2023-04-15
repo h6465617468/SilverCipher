cc168f81826eef5190d9eca06a52281c8abdf499
eenonde <51422382+eenonde@users.noreply.github.com>
2023-04-15 14:05:38 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,27 +1,27 @@
 <?php
-require_once "TreeBox.php";
+require_once "EuclidBox.php";
 $plain_text="Hello World Test 123456789ABC";
 $key="Hello World123456789";
 echo "Plain Text:<br>".$plain_text."<br><br>";
 echo "<br><br>";
-echo "Example TreeBox1 Encrypted Text(1)<br>";
-$ht = new TreeBox1($key);
+echo "Example EuclidBox1 Encrypted Text(1)<br>";
+$ht = new EuclidBox1($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example TreeBox2 Encrypted Text(2)<br>";
-$ht = new TreeBox2($key);
+echo "Example EuclidBox2 Encrypted Text(2)<br>";
+$ht = new EuclidBox2($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example TreeBox3 Encrypted Text(3)<br>";
-$ht = new TreeBox3($key);
+echo "Example EuclidBox3 Encrypted Text(3)<br>";
+$ht = new EuclidBox3($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example TreeBox4 Encrypted Text(4)<br>";
-$ht = new TreeBox4($key);
+echo "Example EuclidBox4 Encrypted Text(4)<br>";
+$ht = new EuclidBox4($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example TreeBox5 Encrypted Text(5)<br>";
-$ht = new TreeBox5($key); // Best
+echo "Example EuclidBox5 Encrypted Text(5)<br>";
+$ht = new EuclidBox5($key); // Best
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
 echo "Decrypted Text:<br>";
