a950ae0f6ac2d8b5b0c90d9c4e9c989dc63f7169
eenonde <51422382+eenonde@users.noreply.github.com>
2023-04-15 15:53:54 +0300

    Update encryption_tool.php

--- encryption_tool.php
+++ encryption_tool.php
@@ -4,18 +4,6 @@
     header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
-    function randomstr($length = 4)
-    {
-        $str = "";
-        $characters = array_merge(range('0','9'));
-        $max = count($characters) - 1;
-        for ($i = 0; $i < $length; $i++)
-        {
-            $rand = mt_rand(0, $max);
-            $str .= $characters[$rand];
-        }
-        return $str;
-    }
 ?>
 <!DOCTYPE html>
 <html>
