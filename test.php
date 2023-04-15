4428eea0b3eeea5e03f1f7a945d7d670830b95c9
eenonde <51422382+eenonde@users.noreply.github.com>
2023-04-15 13:06:54 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,27 +1,27 @@
 <?php
-require_once "HiddenTunnel.php";
+require_once "TreeBox.php";
 $plain_text="Hello World Test 123456789ABC";
 $key="Hello World123456789";
 echo "Plain Text:<br>".$plain_text."<br><br>";
 echo "<br><br>";
-echo "Example HiddenTunnel1 Encrypted Text(1)<br>";
-$ht = new HiddenTunnel1($key);
+echo "Example TreeBox1 Encrypted Text(1)<br>";
+$ht = new TreeBox1($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example HiddenTunnel2 Encrypted Text(2)<br>";
-$ht = new HiddenTunnel2($key);
+echo "Example TreeBox2 Encrypted Text(2)<br>";
+$ht = new TreeBox2($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example HiddenTunnel3 Encrypted Text(3)<br>";
-$ht = new HiddenTunnel3($key);
+echo "Example TreeBox3 Encrypted Text(3)<br>";
+$ht = new TreeBox3($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example HiddenTunnel4 Encrypted Text(4)<br>";
-$ht = new HiddenTunnel4($key);
+echo "Example TreeBox4 Encrypted Text(4)<br>";
+$ht = new TreeBox4($key);
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
-echo "Example HiddenTunnel5 Encrypted Text(5)<br>";
-$ht = new HiddenTunnel5($key); // Best
+echo "Example TreeBox5 Encrypted Text(5)<br>";
+$ht = new TreeBox5($key); // Best
 echo $encrypted_text=$ht->Encrypt($plain_text);
 echo "<br><br>";
 echo "Decrypted Text:<br>";
